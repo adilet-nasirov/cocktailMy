fa4d544b8790cdd028fb50b01f118b98793f4569
adilet-nasirov <adilet.nasirov@yahoo.com>
2021-12-29 22:25:56 -0800

    created project

---
+++ filter.js
@@ -0,0 +1,15 @@
+import {data} from './getData.js'
+
+export const filtered = (searchedName) =>{
+    const arr = data();
+    const filteredCoctails = [];
+    for(let el of arr){
+        let nameOfCoctail = el.strDrink.toLowerCase();
+        if(nameOfCoctail.includes(searchedName.toLowerCase())  ){
+            filteredCoctails.push(el);
+        }
+    }
+    
+    return filteredCoctails
+
+}
